df2784695074a402d54aef93c1da30a63b0a61d6
melodie <josmelodie@gmail.com>
2018-11-04 22:29:10 +1300

    Added Day 5 - For Loops

---
+++ Day_5_Loops/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Day_5_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int sum = n * i;
+                Console.WriteLine($"{n} * {i} = {sum}" );
+            }
+        }
+    }
+}
