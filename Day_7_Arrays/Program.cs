94d9902003e64503f5c95cb28dd3de97c935dba6
melodie <josmelodie@gmail.com>
2018-11-04 21:53:04 +1300

    Add project files.

---
+++ Day_7_Arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day_7_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine()); //the size of the array
+            string[] temp = Console.ReadLine().Split(" ");  //n space-separated ints
+
+            int[] array = Array.ConvertAll(temp, Int32.Parse); //store the temp size into a new array
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+}
